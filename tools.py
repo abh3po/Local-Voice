4aef0d969161f051c5660a970d1ae4b7ae50002b
Abhay <toabhayraizada@gmail.com>
2025-12-04 16:21:42 +0530

    Add Timer

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 # tools.py
 import threading
 import time
+import re
 
 
 def stop_listening_tool(state):
@@ -20,7 +21,7 @@ def start_listening_tool(state, play_sound_fn, sound_file):
 
 
 def set_timer_tool(duration: str, message: str = "Time is up!") -> str:
-    print("Action called, set_timer")
+    print("Action called, set_timer", duration, message)
     try:
         # parse duration string
         pattern = r'(\d+)\s*(s|sec|seconds?|m|min|minutes?|h|hr|hours?)'
